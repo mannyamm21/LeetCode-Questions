9c83a94447a1254f3f0337601beef29f3a3177ef
mannyamm21 <130400515+mannyamm21@users.noreply.github.com>
2024-02-03 00:18:19 +0530

    Time: 0 ms (100.00%), Space: 44.5 MB (40.47%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length < 3) return nums.length;
+        int n=2;
+        for(int i=2;i<nums.length;i++){
+            if(nums[i] != nums[n-2]){
+                nums[n++] = nums[i];
+            }
+        }
+        return n;
+    }
+}
