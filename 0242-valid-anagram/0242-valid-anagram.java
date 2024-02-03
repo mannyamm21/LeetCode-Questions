44dd3d214996e2812dbe49b9a07c241a7b976196
mannyamm21 <130400515+mannyamm21@users.noreply.github.com>
2024-02-03 14:08:49 +0530

    Time: 6 ms (39.20%), Space: 42.9 MB (74.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,18 +1,41 @@
+//using hashmap
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if(s.length() != t.length()){
+//             return false;
+//         }
+//         HashMap<Character,Integer> s1 = new HashMap<>();
+//         HashMap<Character,Integer> t1 = new HashMap<>();
+        
+//         for(char c1 : s.toCharArray()){
+//             s1.put(c1,s1.getOrDefault(c1,0)+1);
+//         }
+        
+//         for(char c2 : t.toCharArray()){
+//             t1.put(c2,t1.getOrDefault(c2,0)+1);
+//         }
+//         return s1.equals(t1);
+//     }
+// }
+
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()){
             return false;
         }
-        HashMap<Character,Integer> s1 = new HashMap<>();
-        HashMap<Character,Integer> t1 = new HashMap<>();
         
-        for(char c1 : s.toCharArray()){
-            s1.put(c1,s1.getOrDefault(c1,0)+1);
+        int[] charArr = new int[26];
+        
+        for(int i=0;i<s.length();i++){
+            charArr[s.charAt(i) - 'a']++;
+            charArr[t.charAt(i) - 'a']--;
         }
         
-        for(char c2 : t.toCharArray()){
-            t1.put(c2,t1.getOrDefault(c2,0)+1);
+        for(int count : charArr){
+            if(count != 0){
+                return false;
+            }
         }
-        return s1.equals(t1);
+        return true;
     }
 }
