6c9a9e1b30fe45dfe9ac33ee2b7854408e66aa6b
mannyamm21 <130400515+mannyamm21@users.noreply.github.com>
2024-02-04 11:49:57 +0530

    0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,22 +1,22 @@
 // Using HashMap
-// class Solution {
-//     public int[] twoSum(int[] nums, int target) {
-//         HashMap<Integer,Integer> map = new HashMap<>();
-//         int [] res = new int[2];
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int [] res = new int[2];
         
-//         for(int i=0;i<nums.length;i++){
-//             if(map.containsKey(nums[i])){
-//                 res[0]=map.get(nums[i]);
-//                 res[1]=i;
-//             }
-//             else{
-//                 map.put(target-nums[i],i);
-//             }
-//         }
-//         return res;
-//     }
-// }
-
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                res[0]=map.get(nums[i]);
+                res[1]=i;
+            }
+            else{
+                map.put(target-nums[i],i);
+            }
+        }
+        return res;
+    }
+}
+//Iteraive solution
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i=0;i<nums.length;i++){
