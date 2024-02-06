25e74b71812c6438da2b5682f381eb0b6249e3b3
mannyamm21 <130400515+mannyamm21@users.noreply.github.com>
2024-02-06 12:29:15 +0530

    Time: 1 ms (100.00%), Space: 53.4 MB (41.77%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,12 +3,12 @@ class Solution {
         int [] res = new int[nums.length];
         
         int prefix = 1;
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<nums.length;i++){ //forward direction
             res[i] = prefix;
             prefix*=nums[i];
         }
         int postfix=1;
-        for(int i=nums.length-1;i>=0;i--){
+        for(int i=nums.length-1;i>=0;i--){ // reverse direction
             res[i]*=postfix;
             postfix*=nums[i];
         }
